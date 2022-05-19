819f0b05fc38f19acb33a081a97e9bd20183cf37
AdamBabud <adam.babud@gmail.com>
2022-05-19 14:04:56 +0200

    fixing CityToVisitController

--- src/main/java/bbdadam/perfinal/bigBOB/controller/CityToVisitController.java
+++ src/main/java/bbdadam/perfinal/bigBOB/controller/CityToVisitController.java
@@ -1,8 +1,24 @@
 package bbdadam.perfinal.bigBOB.controller;
 
+import bbdadam.perfinal.bigBOB.entity.CityToVisit;
+import bbdadam.perfinal.bigBOB.provider.CityToVisitProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CityToVisitController {
+  @Autowired
+  private CityToVisitProvider cityProvider;
 
+  @GetMapping("/cityToVisit")
+  public String greeting(@RequestParam(value = "employeeName", defaultValue="User") String employeeName){
+    CityToVisit city = cityProvider.getCityToVisit(employeeName);
+    if (city == null){
+      return "None of the cities are applicable to visit currently!";
+    } else {
+      return city.getCityName();
+    }
+  }
 }
