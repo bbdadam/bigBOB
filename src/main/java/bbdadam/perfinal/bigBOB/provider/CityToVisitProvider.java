2b37dff73c61d7abfca511e65113866b19de196d
AdamBabud <adam.babud@gmail.com>
2022-05-19 13:49:41 +0200

    adding getCityToVisit and getDistanceBetweenCities

--- src/main/java/bbdadam/perfinal/bigBOB/provider/CityToVisitProvider.java
+++ src/main/java/bbdadam/perfinal/bigBOB/provider/CityToVisitProvider.java
@@ -1,9 +1,16 @@
 package bbdadam.perfinal.bigBOB.provider;
 
+import bbdadam.perfinal.bigBOB.entity.CityToVisit;
+import bbdadam.perfinal.bigBOB.entity.Employee;
 import bbdadam.perfinal.bigBOB.repository.CityToVisitRepository;
 import bbdadam.perfinal.bigBOB.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Component("cityProvider")
 public class CityToVisitProvider {
@@ -19,4 +26,38 @@ public class CityToVisitProvider {
   private EmployeeRepository employeeRepository;
   @Autowired
   private CityToVisitRepository cityToVisitRepository;
+
+  public CityToVisit getCityToVisit(String employeeName){
+    Employee employee = employeeRepository.findEmployeeByName(employeeName).get(0);
+    List<CityToVisit> cities = cityToVisitRepository.findAll();
+    List<CityToVisit> possibleCitiesToVisit = new ArrayList<>();
+
+    return null;
+  }
+  /**
+   * Provides the distance between cities, based on latitude and longitude.
+   * Using the Haversine formula
+   * @param cityA
+   * @return distanceBetweenCitiesInKM
+   */
+  private double getDistanceBetweenCities(CityToVisit cityA){
+    final double RADIUS_OF_EARTH = 6371;
+
+    double latiA = (cityA.getLatitude() * Math.PI) / 180;
+    double lonA = (cityA.getLongitude() * Math.PI) / 180;
+    double latiB = (HQ_LATITUDE * Math.PI) / 180;
+    double lonB = (HQ_LONGITUDE * Math.PI) / 180;
+
+    double dLat = latiB - latiA;
+    double dLon = lonB - lonA;
+
+    double havA = Math.pow(Math.sin(dLat / 2), 2)
+            + Math.cos(latiA) * Math.cos(latiB)
+            * Math.pow(Math.sin(dLon / 2),2);
+
+    double havC = 2 * Math.asin(Math.sqrt(havA));
+
+    return (havC * RADIUS_OF_EARTH);
+  }
+
 }
