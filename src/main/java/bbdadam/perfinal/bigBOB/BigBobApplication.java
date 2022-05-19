e69fcf4c80af7d6fe9e2ae2cd88a22c31db87ded
AdamBabud <adam.babud@gmail.com>
2022-05-19 13:00:38 +0200

    initial commit

---
+++ src/main/java/bbdadam/perfinal/bigBOB/BigBobApplication.java
@@ -0,0 +1,13 @@
+package bbdadam.perfinal.bigBOB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigBobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bbdadam/perfinal/bigBOB/BigBobApplicationTests.java
@@ -0,0 +1,13 @@
+package bbdadam.perfinal.bigBOB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
